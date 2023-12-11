42ae5dafcabdd49c23897e9d392132b9a0785399
PC <PC@DESKTOP-78G6N6I>
2023-12-11 12:39:21 +0800

    111

---
+++ src/Class/DeliveryMenu.java
@@ -0,0 +1,12 @@
+package Class;
+
+import GUI.DeliveryMenuGUI;
+
+public class DeliveryMenu {
+
+    public static void main(String[] args) {
+
+        DeliveryMenuGUI appWindow = new DeliveryMenuGUI("R01");
+    }
+    
+}
